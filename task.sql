6117c1be14dec9e7b8d7621f60f4b9903a8a1684
DariaGera <43523466+DariaGera@users.noreply.github.com>
2022-01-12 17:53:05 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,5 +1,8 @@
-SELECT adverts.category_name, AVG(costs.cost) 
+SELECT table1.category_name
+FROM
+(SELECT adverts.category_name, AVG(costs.cost) as avg_cost
 FROM adverts 
 INNER JOIN costs 
 ON adverts.advert_id=costs.advert_id 
-GROUP BY adverts.category_name
+GROUP BY adverts.category_name) as table1
+WHERE table1.avg_cost < 500
